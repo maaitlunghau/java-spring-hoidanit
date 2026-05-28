558aa12965f533a9467c38e7c7645cd8140e8f3d
maaitlunghau <chunhau.py@gmail.com>
2026-05-28 13:08:25 +0700

    chore: initial java spring starter project - start.spring.io

---
+++ starter-project-initializr/src/main/java/com/example/starter_project_initializr/StarterProjectInitializrApplication.java
@@ -0,0 +1,13 @@
+package com.example.starter_project_initializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterProjectInitializrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterProjectInitializrApplication.class, args);
+	}
+
+}
---
+++ starter-project-initializr/src/test/java/com/example/starter_project_initializr/StarterProjectInitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.starter_project_initializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterProjectInitializrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
