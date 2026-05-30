033ed461d6b485acf6126191cdd062662b76ed44
maaitlunghau <chunhau.py@gmail.com>
2026-05-30 23:01:33 +0700

    feat(04-spring-data): configure mysql database connection

--- 04_spring-data-mvc-crud/src/main/java/vn/hoidanit/springsieutoc/controller/HomeController.java
+++ 04_spring-data-mvc-crud/src/main/java/vn/hoidanit/springsieutoc/controller/HomeController.java
@@ -1,18 +1,14 @@
 package vn.hoidanit.springsieutoc.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-    @Value("${maaitlunghau.secret:default-value}")
-    public String mySecret;
-
     @GetMapping("/")
     public String index() { // RestController
-        return "Hello World from Spring Boot - " + this.mySecret;
+        return "Hello World from Spring Boot - maaitlunghau";
     }
 
     @GetMapping("/dev")
