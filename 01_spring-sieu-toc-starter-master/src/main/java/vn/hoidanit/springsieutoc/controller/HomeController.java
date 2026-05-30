976f7b452e38d0e1ed762240d8d5c8e2606abab1
maaitlunghau <chunhau.py@gmail.com>
2026-05-30 22:00:42 +0700

    feat: demo read configuration in .properties file

--- 01_spring-sieu-toc-starter-master/src/main/java/vn/hoidanit/springsieutoc/controller/HomeController.java
+++ 01_spring-sieu-toc-starter-master/src/main/java/vn/hoidanit/springsieutoc/controller/HomeController.java
@@ -1,14 +1,18 @@
 package vn.hoidanit.springsieutoc.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @Value("${maaitlunghau.secret:default-value}")
+    public String mySecret;
+
     @GetMapping("/")
     public String index() { // RestController
-        return "Hello World from Spring Boot - @hoidanit";
+        return "Hello World from Spring Boot - " + this.mySecret;
     }
 
     @GetMapping("/dev")
