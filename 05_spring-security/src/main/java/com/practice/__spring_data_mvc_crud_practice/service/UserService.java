4124f1e418307aa1b045bdfaa50026e28fa20793
maaitlunghau <chunhau.py@gmail.com>
2026-05-31 16:39:52 +0700

    feat(05-spring-security): add user service

---
+++ 05_spring-security/src/main/java/com/practice/__spring_data_mvc_crud_practice/service/UserService.java
@@ -0,0 +1,49 @@
+package com.practice.__spring_data_mvc_crud_practice.service;
+
+import com.practice.__spring_data_mvc_crud_practice.model.User;
+import com.practice.__spring_data_mvc_crud_practice.repository.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository _userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this._userRepository = userRepository;
+    }
+
+    public Page<User> fetchUsersWithSpec(String keyword, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+
+        if (keyword != null && !keyword.trim().isEmpty()) {
+            return this._userRepository.findByNameContainingOrEmailContaining(keyword, keyword, pageable);
+        }
+
+        return this._userRepository.findAll(pageable);
+    }
+
+    public User createUser(User user) {
+        return this._userRepository.save(user);
+    }
+
+    public Optional<User> findUserById(int id) {
+        return this._userRepository.findById(id);
+    }
+
+    public User updateUser(User user) {
+        if (this._userRepository.existsById(user.getId())) {
+            return this._userRepository.save(user);
+        }
+
+        return null;
+    }
+
+    public void deleteUser(int id) {
+        this._userRepository.deleteById(id);
+    }
+}
