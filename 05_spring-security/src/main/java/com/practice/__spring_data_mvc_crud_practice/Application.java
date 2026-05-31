b9c2b7b115a8dfc1af740d4bed84d9e6f8ca38c8
maaitlunghau <chunhau.py@gmail.com>
2026-05-31 16:01:36 +0700

    feat(05-spring-security): disable authentication default from spring security

--- 05_spring-security/src/main/java/com/practice/__spring_data_mvc_crud_practice/Application.java
+++ 05_spring-security/src/main/java/com/practice/__spring_data_mvc_crud_practice/Application.java
@@ -3,7 +3,11 @@ package com.practice.__spring_data_mvc_crud_practice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		// disable authentication default of Spring Security
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
+
 public class Application {
 
 	public static void main(String[] args) {
