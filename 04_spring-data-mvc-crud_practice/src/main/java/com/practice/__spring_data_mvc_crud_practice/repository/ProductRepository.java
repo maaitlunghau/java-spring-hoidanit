56e1f5b2b5a0f7e864728054b524eccfe108de06
maaitlunghau <chunhau.py@gmail.com>
2026-05-31 12:28:13 +0700

    feat(04-practice): implement findByName method for name-based lookups

--- 04_spring-data-mvc-crud_practice/src/main/java/com/practice/__spring_data_mvc_crud_practice/repository/ProductRepository.java
+++ 04_spring-data-mvc-crud_practice/src/main/java/com/practice/__spring_data_mvc_crud_practice/repository/ProductRepository.java
@@ -4,6 +4,9 @@ import com.practice.__spring_data_mvc_crud_practice.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    Optional<Product> findByName (String name);
 }
