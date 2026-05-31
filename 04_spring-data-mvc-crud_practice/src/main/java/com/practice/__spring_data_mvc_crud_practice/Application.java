6a9e0fb2769224b5e6ffc2787917ef5e99bb2a20
maaitlunghau <chunhau.py@gmail.com>
2026-05-31 11:41:46 +0700

    chore(04-practice): initial new java spring project - spring data practice

---
+++ 04_spring-data-mvc-crud_practice/src/main/java/com/practice/__spring_data_mvc_crud_practice/Application.java
@@ -0,0 +1,13 @@
+package com.practice.__spring_data_mvc_crud_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
