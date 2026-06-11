b63ec2295c174512f4b4269b1e0584f4cc646d93
maaitlunghau <chunhau.py@gmail.com>
2026-06-11 22:17:20 +0700

    feat: implement CustomUserDetailsService and configure DaoAuthenticationProvider

--- 05_spring-security_practice/src/main/java/com/practice/__spring_security_practice/config/SecurityConfiguration.java
+++ 05_spring-security_practice/src/main/java/com/practice/__spring_security_practice/config/SecurityConfiguration.java
@@ -1,7 +1,10 @@
 package com.practice.__spring_security_practice.config;
 
+import com.practice.__spring_security_practice.services.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -10,5 +13,21 @@ public class SecurityConfiguration {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService(UserService userService) {
+        return new CustomUserDetailsService(userService);
+    }
+
+    @Bean
+    DaoAuthenticationProvider daoAuthenticationProvider(
+            UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder
+    ) {
+        DaoAuthenticationProvider dao = new DaoAuthenticationProvider(userDetailsService);
+        dao.setPasswordEncoder(passwordEncoder);
+
+        return dao;
     }
 }
--- 05_spring-security_practice/src/main/java/com/practice/__spring_security_practice/repositories/UserRepository.java
+++ 05_spring-security_practice/src/main/java/com/practice/__spring_security_practice/repositories/UserRepository.java
@@ -4,6 +4,15 @@ import com.practice.__spring_security_practice.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByName(String name);
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByNameAndEmail(String name, String email);
+
+    boolean existsByEmail(String email);
 }
